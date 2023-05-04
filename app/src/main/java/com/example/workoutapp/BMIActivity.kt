2346481be2e9955d8fb2fa10bb0e716e7c549209
Khawaja fakhar <fakhar.oraxtech@gmail.com>
2023-05-04 14:03:38 +0500

    Added radio buttons for changing metrics

--- app/src/main/java/com/example/workoutapp/BMIActivity.kt
+++ app/src/main/java/com/example/workoutapp/BMIActivity.kt
@@ -60,10 +60,7 @@ class BMIActivity : AppCompatActivity() {
         ) {
             bmiLabel = "Normal"
             bmiDescription = "Congratulations! You are in a good shape!"
-        } else if (java.lang.Float.compare(bmi, 25f) > 0 && java.lang.Float.compare(
-                bmi,
-                30f
-            ) <= 0
+        } else if (bmi.compareTo(25f) > 0 && bmi.compareTo(30f) <= 0
         ) {
             bmiLabel = "Overweight"
             bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
