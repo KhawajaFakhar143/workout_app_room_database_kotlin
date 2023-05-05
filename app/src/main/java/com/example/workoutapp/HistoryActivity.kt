d9c3a77cbeab10be59cff82ce74109db5ac5d515
Khawaja fakhar <fakhar.oraxtech@gmail.com>
2023-05-05 18:38:11 +0500

    Used Dao function to get locally stored exercise history and displayed in history screen

--- app/src/main/java/com/example/workoutapp/HistoryActivity.kt
+++ app/src/main/java/com/example/workoutapp/HistoryActivity.kt
@@ -2,7 +2,12 @@ package com.example.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.workoutapp.databinding.ActivityHistoryBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class HistoryActivity : AppCompatActivity() {
     private var binding : ActivityHistoryBinding? = null
@@ -16,6 +21,23 @@ class HistoryActivity : AppCompatActivity() {
         }
         binding?.tbHistory?.setNavigationOnClickListener {
             onBackPressed()
+        }
+        val dao = (application as WorkOutApp).db.getHistoryDao()
+        getHistoryOfExercise(dao)
+
+    }
+    private fun getHistoryOfExercise(dao: HistoryDao){
+        lifecycleScope.launch{
+           dao.getData().collect{
+               var historyList = ArrayList(it)
+               val historyAdaptor =HistoryAdaptor(historyList)
+               withContext(Dispatchers.Main){
+                   binding?.rvHistory?.layoutManager = LinearLayoutManager(
+                       this@HistoryActivity,LinearLayoutManager.VERTICAL,false)
+                   binding?.rvHistory?.adapter = historyAdaptor
+               }
+           }
+
         }
     }
 }
